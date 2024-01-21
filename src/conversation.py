a089e700071668e1cc44ee58f02117d4c4edc39d
Mauricio Ulargui <ulargm@amazon.com>
2024-01-21 11:31:11 -0800

    testing

--- src/conversation.py
+++ src/conversation.py
@@ -1,14 +1,16 @@
 from basic_ai import *
 from chat import *
 
-print(list_models())
-print(ask(prompt = "Hi, how are you?"))
-print(ask(prompt = "Tell me a funny joke"))
-print(ask(prompt = "Which is the capital is Spain?"))
+#print(list_models())
+
+#print(ask(prompt = "Hi, how are you?"))
+#print(ask(prompt = "Tell me a funny joke"))
+#print(ask(prompt = "Which is the capital is Spain?"))
 print(ask(prompt = "The population fo the US is..."))
 
 chat = Chat()
 print(chat.ask(prompt = "Hi, how are you?"))
 chat.ask(prompt = "Tell me a funny joke")
+chat.restart()
 chat.ask(prompt = "Which is the capital is Spain?")
 chat.ask(prompt = "The population fo the US is...")
