3f4a8676d78f01c8c91815821c9317daf1b17e06
Mauricio Ulargui <contact@ulargui.com>
2024-02-19 19:52:14 +0000

    adding debugging

--- debug.sh
+++ debug.sh
@@ -1,1 +1,5 @@
 ./infra/run.sh debug
+
+# In case the container is running remote (ie in a cloud VM) the easiest solution
+# is to create a ssh tunnel that routes all the traffic in a local port to the remote server
+#    ssh -L 5678:localhost:5678 username@myremoteserver.com
